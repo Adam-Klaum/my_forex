7b53fd1d14ee9064ebfbfa8793805e655bc484a7
Adam Klaum <adam.w.klaum@gmail.com>
2018-05-28 11:36:23 -0400

    created Oanda config reader

---
+++ oa_config.py
@@ -0,0 +1,21 @@
+import yaml
+from pathlib import Path
+
+class OAConf:
+
+    def __init__(self):
+
+        home = str(Path.home())
+
+        with open(home + "/v20.conf", 'r') as stream:
+            try:
+                conf_dict = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+        self.c_list = []
+
+        for key, value in conf_dict.items():
+            setattr(self, key, value)
+            self.c_list.append(key)
+
